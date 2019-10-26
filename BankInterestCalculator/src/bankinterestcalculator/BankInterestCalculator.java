00559dd29d51cc3324110d9d6763333287aa70d7
sourojyotipaul <51042496+sourojyotipaul@users.noreply.github.com>
2019-10-26 15:39:06 +0530

    Add files via upload

---
+++ BankInterestCalculator/src/bankinterestcalculator/BankInterestCalculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankinterestcalculator;
+
+/**
+ *
+ * @author Subhojit
+ */
+public class BankInterestCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main m=new Main();
+        m.show();
+    }
+    
+}
